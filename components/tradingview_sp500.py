98324f58ce0bb51dfa979b95704392648169392d
GW-LHM <gwendal.lhermite@outlook.fr>
2026-01-07 19:05:58 +0100

    Create tradingview_sp500.py

---
+++ components/tradingview_sp500.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+def render_tradingview_sp500():
+    st.subheader("📈 S&P 500 (TradingView)")
+
+    tradingview_html = """
+    <div class="tradingview-widget-container">
+      <div id="tradingview_spx"></div>
+      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+      <script type="text/javascript">
+      new TradingView.widget({
+        "width": "100%",
+        "height": 500,
+        "symbol": "SP:SPX",
+        "interval": "M",
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "1",
+        "locale": "fr",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "hide_side_toolbar": false,
+        "allow_symbol_change": false,
+        "container_id": "tradingview_spx"
+      });
+      </script>
+    </div>
+    """
+
+    components.html(tradingview_html, height=550)
