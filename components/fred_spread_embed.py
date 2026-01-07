7bce2226861a1715e0c06eeb696fb4ddb424e6c3
GW-LHM <gwendal.lhermite@outlook.fr>
2026-01-07 21:31:12 +0100

    Update fred_spread_embed.py

--- components/fred_spread_embed.py
+++ components/fred_spread_embed.py
@@ -1,17 +1,13 @@
-import streamlit as st
 import streamlit.components.v1 as components
 
-
-def render_fred_spread():
-    st.subheader("📉 Spread des taux US 10Y – 3M (source FRED)")
-
-    iframe = """
-    <iframe
-        src="https://fred.stlouisfed.org/graph/graph-landing.php?g=1YfH"
-        width="100%"
-        height="500"
-        style="border:0;">
-    </iframe>
+components.html(
     """
-
-    components.html(iframe, height=520)
+    <iframe
+      src="https://fred.stlouisfed.org/graph/?id=T10Y3M"
+      width="100%"
+      height="520"
+      style="border:0;">
+    </iframe>
+    """,
+    height=540
+)
